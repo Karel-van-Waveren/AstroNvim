6f1d0060e795c100f997293121159e4478b4f41b
karel <karljf@gmail.com>
2023-04-16 22:21:42 +0200

    init

---
+++ init.lua
@@ -0,0 +1,10 @@
+return {
+  background = "dark", -- or "light" for light mode
+  colorscheme = "gruvbox",
+  -- -- vim.cmd.colorscheme "catppuccin-mocha"
+  --
+  --- notify fix
+  notify = {
+    background_colour = "#000000",
+  },
+}
