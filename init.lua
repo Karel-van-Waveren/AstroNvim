38ce8636e525aeed244d5f029d2f494a0c172f01
karel <karljf@gmail.com>
2023-04-18 01:10:25 +0200

    gruvbox material

--- init.lua
+++ init.lua
@@ -1,7 +1,5 @@
 return {
-  background = "dark", -- or "light" for light mode
-  colorscheme = "gruvbox",
-  -- -- vim.cmd.colorscheme "catppuccin-mocha"
-  --
-  --- notify fix
+  -- colorscheme = "gruvbox",
+  -- colorscheme "catppuccin-mocha"
+  colorscheme = "gruvbox-material",
 }
---
+++ plugins/gruvbox-material.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "sainnhe/gruvbox-material",
+    opts = function()
+      vim.g.gruvbox_material_transparent_background = "1"
+      vim.g.gruvbox_material_background = "hard"
+    end,
+  },
+}
