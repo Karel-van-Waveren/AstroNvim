35526966c07e84107964bcbff30a9aa9d5c91ccb
karel <kareljf@gmail.com>
2023-05-01 19:18:38 +0200

    mapping and theme cleanup

--- init.lua
+++ init.lua
@@ -1,12 +1,7 @@
 return {
-  -- colorscheme = "gruvbox",
-  -- colorscheme = "catppuccin",
   -- colorscheme = "gruvbox-material",
-  -- colorscheme = "oxocarbon",
-  -- colorscheme = "rose-pine"
-  -- colorscheme = "nightfox"
   colorscheme = "kanagawa",
-  -- colorscheme = "tokyonight",
+  -- colorscheme = "monokai-pro",
   lsp = {
     capabilities = {
       textDocument = {
