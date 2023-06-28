edecdf845bf0cd06606fa89ef94f3ec387e68fb4
karel <kareljf@gmail.com>
2023-06-28 23:11:06 +0200

    remove noice

--- plugins/community.lua
+++ plugins/community.lua
@@ -35,7 +35,7 @@ return {
   { import = "astrocommunity.colorscheme.mellow-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.utility.transparent-nvim" },
-  { import = "astrocommunity.utility.noice-nvim" },
+  -- { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
   { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
   { import = "astrocommunity.project.nvim-spectre" },
