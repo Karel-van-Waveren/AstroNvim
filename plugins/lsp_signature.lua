00648e107ed44b64a4576582a4f46280090f6d71
karel <kareljf@gmail.com>
2024-02-24 17:40:41 +0100

    signature

--- plugins/lsp_signature.lua
+++ plugins/lsp_signature.lua
@@ -1,15 +1,15 @@
 return {
-  -- "ray-x/lsp_signature.nvim",
-  -- event = "BufRead",
-  -- config = function()
-  --   require("lsp_signature").setup {
-  --     doc_lines = 0,
-  --     floating_window = true,
-  --     hint_prefix = "> ",
-  --     handler_opts = {
-  --       border = "rounded",
-  --     },
-  --   }
-  -- end,
-  -- opts = {},
+  "ray-x/lsp_signature.nvim",
+  event = "BufRead",
+  config = function()
+    require("lsp_signature").setup {
+      doc_lines = 0,
+      floating_window = true,
+      hint_prefix = "> ",
+      handler_opts = {
+        border = "rounded",
+      },
+    }
+  end,
+  opts = {},
 }
