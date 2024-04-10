5cc74a3d8f44a2415ae6e2daa3b62adce442b502
karel <kareljf@gmail.com>
2024-04-11 01:07:39 +0200

    fix rust pack

--- lua/community.lua
+++ lua/community.lua
@@ -17,34 +17,6 @@ return {
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
-  {
-    "simrat39/rust-tools.nvim",
-    opts = {
-      tools = {
-        hover_actions = {
-          auto_focus = true,
-        },
-        inlay_hints = {
-          auto = false,
-        },
-      },
-      server = {
-        on_attach = function(client, bufnr)
-          -- override here. call lsp on attach and then add own custom logic.
-          require("astronvim.utils.lsp").on_attach(client, bufnr)
-          local rt = require "rust-tools"
-
-          local utils = require "astronvim.utils"
-
-          utils.set_mappings({
-            n = {
-              ["K"] = { rt.hover_actions.hover_actions, desc = "Rust Hover Actions" },
-            },
-          }, { buffer = bufnr })
-        end,
-      },
-    },
-  },
   { import = "astrocommunity.colorscheme.kanagawa-nvim" },
   { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
   { import = "astrocommunity.colorscheme.gruvbox-baby" },
