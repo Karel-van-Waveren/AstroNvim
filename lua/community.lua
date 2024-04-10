40f3260ba1f5c421e4ee47231df152494da9facf
karel <kareljf@gmail.com>
2024-04-11 00:45:10 +0200

    cleaned up community

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -8,5 +8,47 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  {
+    "simrat39/rust-tools.nvim",
+    opts = {
+      tools = {
+        hover_actions = {
+          auto_focus = true,
+        },
+        inlay_hints = {
+          auto = false,
+        },
+      },
+      server = {
+        on_attach = function(client, bufnr)
+          -- override here. call lsp on attach and then add own custom logic.
+          require("astronvim.utils.lsp").on_attach(client, bufnr)
+          local rt = require "rust-tools"
+
+          local utils = require "astronvim.utils"
+
+          utils.set_mappings({
+            n = {
+              ["K"] = { rt.hover_actions.hover_actions, desc = "Rust Hover Actions" },
+            },
+          }, { buffer = bufnr })
+        end,
+      },
+    },
+  },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
+  { import = "astrocommunity.colorscheme.gruvbox-baby" },
+  { import = "astrocommunity.colorscheme.mellow-nvim" },
+  { import = "astrocommunity.colorscheme.melange-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
 }
